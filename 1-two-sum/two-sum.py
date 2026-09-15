676215f1d6afae83996d5b8e9f901e9e6f2c3843
TITHEE05 <144910842+TITHEE05@users.noreply.github.com>
2026-09-15 16:22:05 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (95.26%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,11 +5,18 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        op = []
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    op.append(i)
-                    op.append(j)
-                    return op
+        #attempt 2: dictionary
+        #create empty dictionary
+        d = {}
+        #enumerate function helps return index as well as value from a list
+        for i, num in enumerate(nums):
+            #find complement 
+            comp = target - num
+            if comp in d:
+                return [d[comp], i] #d[comp] returns index since in output we wnat index from the original array
+            else:
+                #i holds the index of the value from the original array
+                d[num] = i
+        
+        return False
         
